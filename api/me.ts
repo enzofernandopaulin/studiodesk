09011baf4a5b6464fbedec145c9d78f7f0787dba
Enzo Fernando Paulin <enzopaulin180@gmail.com>
2026-09-06 15:45:34 -0300

    Corrige resolução ESM nas funções da Vercel

--- api/me.ts
+++ api/me.ts
@@ -1,6 +1,6 @@
-import { authenticateRequest, getMembership } from './_lib/supabaseAdmin';
-import { json, methodNotAllowed, serverError } from './_lib/http';
-import { rateLimit } from './_lib/security';
+import { authenticateRequest, getMembership } from './_lib/supabaseAdmin.js';
+import { json, methodNotAllowed, serverError } from './_lib/http.js';
+import { rateLimit } from './_lib/security.js';
 
 export default async function handler(request: Request): Promise<Response> {
   if (request.method !== 'GET') return methodNotAllowed(['GET']);
